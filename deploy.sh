c22b03c2eb87a9f7fd16f4a6852c0ff768f965c4
James Bengeyfield <james.bengeyfield@gmx.com>
2012-04-24 21:36:50 +0100

    Created OS agnostic deploy/update scripts

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,5 @@
-#/bin/sh
-for version in "11" "10"
-do
-  dir="`echo ~/Library/Preferences/IntelliJIdea$version/colors`"
-  if [ -e "$dir" ]
-  then
-    cp BlueForest.xml $dir
-    break
-  fi
-  echo "IntelliJ directory was not found"
-done
+#/bin/bash
+export mode="deploy"
+
+./updateOrDeploy.sh
 
--- update.sh
+++ update.sh
@@ -1,11 +1,5 @@
-#/bin/sh
-for version in "11" "10"
-do
-  dir="`echo ~/Library/Preferences/IntelliJIdea$version/colors`"
-  if [ -e "$dir" ]
-  then
-    cp $dir/BlueForest.xml .
-    break
-  fi
-done
+#/bin/bash
+export mode="update"
+
+./updateOrDeploy.sh
 
